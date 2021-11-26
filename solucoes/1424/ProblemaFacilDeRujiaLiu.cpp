8b3582655905aaa23427e6378378f2e8fb0c9c62
Rafael <rafael09felipe@gmail.com>
2021-11-26 10:23:29 -0300

    Create ProblemaFacilDeRujiaLiu.cpp

---
+++ solucoes/1424/ProblemaFacilDeRujiaLiu.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+ 
+const int MAXN = 1E6;
+ 
+int vetor[MAXN];
+ 
+int main() {
+    int n = 0, m = 0, k = 0, v = 0;
+    map<int, vector<int>> mymap;
+    int number = 0;
+ 
+    while(cin >> n >> m) {
+        for(int i = 0; i < n; i++) {
+            cin >> number;
+            mymap[number].push_back(i+1);
+        }
+ 
+        for(int i = 0; i < m; i++) {
+            cin >> k >> v;
+            if(mymap.find(v) != mymap.end()) {
+                if(mymap[v].size() >= k) {
+                    cout << mymap[v].at(k-1) << endl; 
+                }
+                else cout << 0 << endl;
+            }
+            else cout << 0 << endl;
+        }
+        mymap.clear();
+    }
+ 
+	return 0;
+}
