8df41befbcf2e3fe522346608f9be2bb229b4ac5
rfps09 <rafael09felipe@gmail.com>
2022-02-13 23:01:40 -0300

    time limit

---
+++ solucoes/2969/Interplanetario.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <climits>
+
+using namespace std;
+using pii = pair<int,int>;
+
+const int MAXN = 401;
+const int INF = INT_MAX/2;
+
+int n,r,q;
+int x,y,d;
+int a,b,k,t;
+vector<pii> grafinho[MAXN];
+map<int,vector<int>> graus;
+pii temps[MAXN];
+pii floyd[MAXN][MAXN][MAXN];
+
+int main() {
+    freopen("entrada.txt", "r", stdin);
+    freopen("saida.txt", "w", stdout);
+
+    cin >> n >> r;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            for(int l = 1; l <= n; l++){
+                if(i != j) {
+                    floyd[i][j][l].first = INF;
+                    floyd[i][j][l].second = INF;
+                }
+            }
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        cin >> x;
+        graus[x].push_back(i);
+    }
+
+    map<int,vector<int>>::iterator it;
+    int count = 0;
+    int rcount = graus.size();
+
+    for(it = graus.begin(); it != graus.end(); it++) {
+        count++;
+        for(int i = 0; i < it->second.size(); i++) {
+            temps[it->second[i]].first = count;
+            temps[it->second[i]].second = rcount;
+        }
+        rcount--;
+    }
+
+    for(int i = 0; i < r; i++) {
+        cin >> x >> y >> d;
+        grafinho[x].push_back(pii(y,d));
+        grafinho[y].push_back(pii(x,d));
+        for(int j = 1; j <= n; j++) {
+            floyd[x][y][j].first = d;
+            floyd[y][x][j].first = d;
+            floyd[x][y][j].second = d;
+            floyd[y][x][j].second = d;
+        }
+    }
+
+    for(int z = 1; z <= n; z++) 
+    {
+        for(int i = 1; i <= n; i++) 
+        {
+            for(int j = 1; j <= n; j++) 
+            {
+                for(int l = 1; l <= n; l++) 
+                {
+                    if(temps[z].first <= l) 
+                    {
+                        if(floyd[i][j][l].first > floyd[i][z][l].first + floyd[z][j][l].first) 
+                        floyd[i][j][l].first = floyd[i][z][l].first + floyd[z][j][l].first;
+                    }
+                    if(temps[z].second <= l) 
+                    {
+                        if(floyd[i][j][l].second > floyd[i][z][l].second + floyd[z][j][l].second) 
+                        floyd[i][j][l].second = floyd[i][z][l].second + floyd[z][j][l].second;
+                    }   
+                }
+            }
+        }
+    }
+
+    cin >> q;
+    
+    for(int i = 0; i < q; i++) {
+        cin >> a >> b >> k >> t;
+
+        if(t && floyd[a][b][k].second != INF)
+            cout << floyd[a][b][k].second << "\n";
+        else if(!t && floyd[a][b][k].first != INF)
+            cout << floyd[a][b][k].first << "\n";
+        else cout << "-1" << "\n";
+    }
+
+    fclose(stdin);
+    fclose(stdout);
+
+    return 0;
+}
